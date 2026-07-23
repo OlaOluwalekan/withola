46669f9b02c24ff0e775050e0a4e60ba62ea0c20
OlaOluwalekan <olalekanbello534@gmail.com>
2026-07-23 13:30:41 +0100

    feat: implement admin dashboard layout with responsive sidebar navigation

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -24,11 +24,11 @@ export default function RootLayout({
         className={`${GeistSans.className} bg-white dark:bg-black text-black dark:text-white`}
       >
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-          <div className='flex min-h-screen'>
+          <div className='flex h-screen overflow-hidden'>
             <Sidebar />
-            <div className='flex-1 flex flex-col'>
+            <div className='flex-1 flex flex-col min-w-0 pb-16 md:pb-0 h-screen'>
               <Header />
-              <main className='flex-1 p-6 overflow-auto'>{children}</main>
+              <main className='flex-1 p-4 md:p-6 overflow-y-auto'>{children}</main>
             </div>
           </div>
           <Toaster richColors position='top-right' />
